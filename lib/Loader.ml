fe83a10b22792224ac376b7543e045cbeba1c0c6
cedlemo <cedlemo@gmx.com>
2018-04-29 15:05:17 +0200

    Loader : simplify write_bindings_for function and fix issue with Repository.require

--- lib/Loader.ml
+++ lib/Loader.ml
@@ -209,28 +209,59 @@ let parse loader
   done;
   Binding_utils.Sources.close main_sources
 
-let generate_files loader name =
+let generate_files name =
   let module_name = Lexer.snake_case name in
   Binding_utils.Sources.create_ctypes module_name
 
-let rec write_bindings_for loader = function
+let rec write_bindings_for namespace = function
   | [] -> ()
   | name :: others ->
       let _ =  begin
-        match Repository.find_by_name name loader.namespace with
-        | None ->
-            let strs =  ["Unable to find"; name; "in the namespace"; loader.namespace] in
-            let message = String.concat " " strs  in
-            print_endline  message
-        | Some bi ->
-            if Base_info.is_deprecated bi then
-              let message = name ^ " is deprecated" in
-              print_endline message
-            else begin
-              let open Binding_utils in
-              let sources = generate_files loader name in
-
-              Sources.close sources
-            end;
-      end
-      in write_bindings_for loader others
+        match Repository.require namespace () with
+        | Error message -> print_endline message
+        | Ok typelib ->
+          match Repository.find_by_name namespace name with
+          | None ->
+              let strs =  ["Unable to find"; name; "in the namespace"; namespace] in
+              let message = String.concat " " strs  in
+              print_endline  message
+          | Some bi ->
+              if Base_info.is_deprecated bi then
+                let message = name ^ " is deprecated" in
+                print_endline message
+              else begin
+                let open Binding_utils in
+                let sources = generate_files name in
+                match Base_info.get_type bi with
+                | Base_info.Boxed | Base_info.Struct -> begin
+                    let si = Struct_info.from_baseinfo bi in
+                    if Struct_info.is_gtype_struct si then ()
+                    else
+                      Bind_struct.parse_struct_info bi sources []
+                  end
+                | Base_info.Enum ->
+                    Bind_enum.parse_enum_info bi sources
+                | Base_info.Flags ->
+                    Bind_enum.parse_flags_info bi sources
+                | Base_info.Constant ->
+                    Bind_constant.parse_constant_info bi sources
+                | Base_info.Union ->
+                    Bind_union.parse_union_info bi sources []
+                | Base_info.Callback -> ()
+                | Base_info.Invalid -> ()
+                | Base_info.Value -> ()
+                | Base_info.Signal -> ()
+                | Base_info.Vfunc -> ()
+                | Base_info.Property -> ()
+                | Base_info.Field -> ()
+                | Base_info.Arg -> ()
+                | Base_info.Type -> ()
+                | Base_info.Unresolved -> ()
+                | Base_info.Object ->
+                    Bind_object.parse_object_info bi sources []
+                | Base_info.Invalid_0 -> ()
+                | Base_info.Interface -> ()
+                | _ -> ()
+              end;
+        end
+        in write_bindings_for namespace others
