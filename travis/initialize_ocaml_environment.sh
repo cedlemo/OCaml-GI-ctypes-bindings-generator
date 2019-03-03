a509d063c017bab0d25f959ed5368ce0da2b5346
Cédric Le Moigne <cedlemo@gmx.com>
2019-03-03 09:14:06 +0100

    Install gobject-introspection package as dependency

--- travis/initialize_ocaml_environment.sh
+++ travis/initialize_ocaml_environment.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 opam install ctypes configurator base stdio ctypes-foreign jbuilder dune odoc bisect_ppx ounit
+opam pin add -y gobject-introspection https://github.com/cedlemo/OCaml-GObject-Introspection.git
