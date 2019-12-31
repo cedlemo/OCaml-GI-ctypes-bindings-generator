d7c88bfd2ba0b316eae21f91293c25c7d3c92138
cedlemo <cedlemo@gmx.com>
2019-12-31 18:01:57 +0100

    Travis: update and upgrade opam packages

--- travis/initialize_ocaml_environment.sh
+++ travis/initialize_ocaml_environment.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-opam install ctypes configurator base stdio ctypes-foreign jbuilder dune odoc bisect_ppx ounit
-opam pin add -y gobject-introspection https://github.com/cedlemo/OCaml-GObject-Introspection.git
+opam update
+opam upgrade -y
+opam install ctypes configurator base stdio ctypes-foreign jbuilder dune odoc bisect_ppx ounit gobject-introspection
