79356979102579fb0df01a527e31711dfa0c8d74
cedlemo <cedlemo@gmx.com>
2020-01-01 10:13:58 +0100

    opam pin conf-gobject-introspection

--- travis/initialize_ocaml_environment.sh
+++ travis/initialize_ocaml_environment.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 opam install ctypes configurator base stdio ctypes-foreign jbuilder dune odoc bisect_ppx ounit
+opam pin add -y conf-gobject-introspection https://github.com/cedlemo/conf-gobject-Introspection.git
 opam pin add -y gobject-introspection https://github.com/cedlemo/OCaml-GObject-Introspection.git
